dedd5c17995eb3466b24f5e9243c1b6fd048c3bd
Dmitry Vasilyev <vadv.mkn@gmail.com>
2017-07-05 15:35:37 +0300

    init

---
+++ rsa/rsa.go
@@ -0,0 +1,89 @@
+package rsa
+
+import (
+	"crypto/rsa"
+	"fmt"
+	"math/big"
+)
+
+// pure-go realization of RSA_public_decrypt
+func PublicDecrypt(pubKey *rsa.PublicKey, data []byte) ([]byte, error) {
+	c := new(big.Int)
+	m := new(big.Int)
+	m.SetBytes(data)
+	e := big.NewInt(int64(pubKey.E))
+	c.Exp(m, e, pubKey.N)
+	out := c.Bytes()
+	skip := 0
+	for i := 2; i < len(out); i++ {
+		if i+1 >= len(out) {
+			break
+		}
+		if out[i] == 0xff && out[i+1] == 0 {
+			skip = i + 2
+			break
+		}
+	}
+
+	return out[skip:], nil
+}
+
+// pure-go realization of RSA_private_encrypt
+func PrivateEncrypt(priv *rsa.PrivateKey, data []byte) (enc []byte, err error) {
+	k := (priv.N.BitLen() + 7) / 8
+	tLen := len(data)
+	// rfc2313, section 8: The length of the data D shall not be more than k-11 octets
+	if tLen > k-11 {
+		err = fmt.Errorf("input size")
+		return
+	}
+	em := make([]byte, k)
+	em[1] = 1
+	for i := 2; i < k-tLen-1; i++ {
+		em[i] = 0xff
+	}
+	copy(em[k-tLen:k], data)
+	c := new(big.Int).SetBytes(em)
+	if c.Cmp(priv.N) > 0 {
+		err = fmt.Errorf("encryption")
+		return
+	}
+	var m *big.Int
+	var ir *big.Int
+	if priv.Precomputed.Dp == nil {
+		m = new(big.Int).Exp(c, priv.D, priv.N)
+	} else {
+		// We have the precalculated values needed for the CRT.
+		m = new(big.Int).Exp(c, priv.Precomputed.Dp, priv.Primes[0])
+		m2 := new(big.Int).Exp(c, priv.Precomputed.Dq, priv.Primes[1])
+		m.Sub(m, m2)
+		if m.Sign() < 0 {
+			m.Add(m, priv.Primes[0])
+		}
+		m.Mul(m, priv.Precomputed.Qinv)
+		m.Mod(m, priv.Primes[0])
+		m.Mul(m, priv.Primes[1])
+		m.Add(m, m2)
+
+		for i, values := range priv.Precomputed.CRTValues {
+			prime := priv.Primes[2+i]
+			m2.Exp(c, values.Exp, prime)
+			m2.Sub(m2, m)
+			m2.Mul(m2, values.Coeff)
+			m2.Mod(m2, prime)
+			if m2.Sign() < 0 {
+				m2.Add(m2, prime)
+			}
+			m2.Mul(m2, values.R)
+			m.Add(m, m2)
+		}
+	}
+
+	if ir != nil {
+		// Unblind.
+		m.Mul(m, ir)
+		m.Mod(m, priv.N)
+	}
+	enc = m.Bytes()
+	return
+}
